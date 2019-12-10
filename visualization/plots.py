1df757d391627385da39eaa096b749d029b42ede
Manu <mshalvagal@gmail.com>
2019-12-10 17:29:56 +0100

    Started to move plotting functions to package

--- visualization/plots.py
+++ visualization/plots.py
@@ -80,3 +80,67 @@ def plot_learning_curves(parent_dir, mode='mean', log_interval=20, teacher_dir=N
     plt.legend()
     plt.subplot(2, 1, 2)
     plt.legend()
+
+
+def scrollable_loss_vs_hist(parent_dir, run_num, idxes=None, ylim=None, net2net=False, steps=1):
+    
+    x = np.load(os.path.join(parent_dir, 'cos_dists_' + str(run_num) + '.npy'))
+    
+    offset = 0
+
+    loss_curve = np.load(parent_dir + 'loss_curve_' + str(run_num) + '.npy')
+    
+    num_neurons = x.shape[1]
+    iu2 = np.triu_indices(num_neurons, k=1)
+    
+    if idxes is None:
+        if net2net:
+            idxes = np.arange(40)
+            offset = 40
+        else:
+            idxes = np.arange(80)
+
+    batch_idxes = [46*(i + offset) for i in idxes]
+
+    # now the real code :) 
+    global curr_pos
+    curr_pos = 0
+
+    def key_event(e):
+        global curr_pos
+
+        if e.key == "right":
+            curr_pos = curr_pos + steps
+        elif e.key == "left":
+            curr_pos = curr_pos - steps
+        else:
+            return
+        curr_pos = curr_pos % len(x)
+
+
+        ax1.cla()
+        ax1.plot(loss_curve)
+        ax1.plot(curr_pos + offset*46, loss_curve[curr_pos + offset*46], marker='o', label='batch ' + str(curr_pos))
+        ax1.legend()
+
+        ax2.cla()
+        plots = x[curr_pos]
+        ax2.hist(plots[iu2], bins=20)
+        ax2.set_xlim([-1, 1])
+        if ylim is not None:
+            ax2.set_ylim([0, ylim])
+        fig.canvas.draw()
+
+    fig = plt.figure()
+    fig.canvas.mpl_connect('key_press_event', key_event)
+    ax1 = fig.add_subplot(211)
+    ax1.plot(loss_curve)
+    ax1.plot(1 + offset*46, loss_curve[1 + offset*46], marker='o')
+
+    ax2 = fig.add_subplot(212)
+    plots = x[0]
+    ax2.hist(plots[iu2], bins=20)
+    ax2.set_xlim([-1, 1])
+    if ylim is not None:
+        ax2.set_ylim([0, ylim])
+    plt.show()   
